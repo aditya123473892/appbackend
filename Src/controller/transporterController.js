202da5e72f48854e7cae01e56dbbe665b6d60f55
aditya123473892 <adityathakur6199@gmail.com>
2025-09-30 20:21:27 +0530

    chnages

--- Src/controller/transporterController.js
+++ Src/controller/transporterController.js
@@ -1,10 +1,12 @@
 const Transporter = require("../models/TransporterModel");
 
 exports.getAllTransporters = async (req, res) => {
-    try {
-        const transporters = await Transporter.getAll();
-        res.json(transporters);
-    } catch (err) {
-        res.status(500).json({ message: "Error fetching transporters", error: err });
-    }
+  try {
+    const transporters = await Transporter.getAll();
+    res.json(transporters);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error fetching transporters", error: err });
+  }
 };
